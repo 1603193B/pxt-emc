33528545ee1e4f436aa5fa34f5d1c62a3986d260
1603193B <37626875+1603193B@users.noreply.github.com>
2019-01-09 22:27:21 +0800

    Added a testMotor function

--- main.ts
+++ main.ts
@@ -1,17 +1,38 @@
+// https://makecode.com/extensions/getting-started
+// https://makecode.com/defining-blocks
+
 //% color=190 weight=100 icon="\uf1ec" block="EMC"
 namespace EMC {
     export namespace mBot {
         export function leftMotor() {
-
+            //TODO
         }
 
-        function rightMotor() {
-
+        export function rightMotor() {
+            //TODO
         }
-    }
 
-    //% block
-    export function helloWorld() {
-        basic.showString("Hello!")
+        export function testMotor() {
+            led.toggleAll()
+
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 100)
+            basic.pause(1000)
+            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
+            basic.pause(1000)
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 100)
+            basic.pause(1000)
+            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
+            basic.pause(1000)
+
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 100)
+            basic.pause(1000)
+            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
+            basic.pause(1000)
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 100)
+            basic.pause(1000)
+            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
+            basic.pause(1000)
+            led.toggleAll()
+        }
     }
 }
