811d53d7608a420a8a328f69809669d574ccf21c
1603193B <37626875+1603193B@users.noreply.github.com>
2019-01-09 22:43:07 +0800

    Implemented leftMotor and rightMotor functions

--- main.ts
+++ main.ts
@@ -1,17 +1,55 @@
+// https://makecode.com/extensions/getting-started
+// https://makecode.com/defining-blocks
+
 //% color=190 weight=100 icon="\uf1ec" block="EMC"
 namespace EMC {
     export namespace mBot {
-        export function leftMotor() {
-
+        export const enum directions {
+            Invalid = -2,
+            None = -1,
+            North = 0,
+            NorthEast = 1,
+            Clockwise = 2,
+            SouthEast = 3,
+            South = 4,
+            SouthWest = 5,
+            AntiClockwise = 6,
+            NorthWest = 7
         }
 
-        function rightMotor() {
+        export const radioKeyword: string = "DIR"
 
+        // Assumes left motor to be Motor1
+        export function leftMotor(motorDir: kitronik_motor_driver.MotorDirection, speed: number) {
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, motorDir, speed)
         }
-    }
 
-    //% block
-    export function helloWorld() {
-        basic.showString("Hello!")
+        // Assume right motor ti be Motor2
+        export function rightMotor(motorDir: kitronik_motor_driver.MotorDirection, speed: number) {
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, motorDir, speed)
+        }
+
+        export function testMotors() {
+            led.toggleAll()
+
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 100)
+            basic.pause(5000)
+            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
+            basic.pause(1000)
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 100)
+            basic.pause(5000)
+            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
+            basic.pause(1000)
+
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 100)
+            basic.pause(5000)
+            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
+            basic.pause(1000)
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 100)
+            basic.pause(5000)
+            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
+            basic.pause(1000)
+            led.toggleAll()
+        }
     }
 }
