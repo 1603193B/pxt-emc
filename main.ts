a3a4e3bc52041fbeb94e877cb7cdaa54161ef80e
1603193B <37626875+1603193B@users.noreply.github.com>
2019-01-09 22:54:17 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -4,12 +4,32 @@
 //% color=190 weight=100 icon="\uf1ec" block="EMC"
 namespace EMC {
     export namespace mBot {
-        export function leftMotor() {
-            //TODO
+        export const enum directions {
+            Invalid = -2,
+            None = -1,
+            North = 0,
+            NorthEast = 1,
+            Clockwise = 2,
+            SouthEast = 3,
+            South = 4,
+            SouthWest = 5,
+            AntiClockwise = 6,
+            NorthWest = 7
         }
 
-        export function rightMotor() {
-            //TODO
+        //%block
+        export const radioKeyword: string = "DIR"
+
+        // Assumes left motor to be Motor1
+        //% speed.min=0
+        export function leftMotor(motorDir: kitronik_motor_driver.MotorDirection, speed: number) {
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, motorDir, speed)
+        }
+
+        // Assume right motor ti be Motor2
+        //% speed.min=0
+        export function rightMotor(motorDir: kitronik_motor_driver.MotorDirection, speed: number) {
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, motorDir, speed)
         }
 
         export function testMotor() {
